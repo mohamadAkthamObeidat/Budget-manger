8d5bd99460269ad9fe380068f84877c1d028b7ee
Mohamad Aktham Obeidat <mohamadaktham7@gmail.com>
2019-09-09 12:30:45 +0300

    Conflicts Resolved

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,13 +2,14 @@ import React from "react";
 import "./App.css";
 import Sidebar from "./components/dashboard/sidebar";
 import Main from "./components/login/Main";
-
+import Dashboard from './components/dashboard/Dashboard'
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 function App() {
   return (
     <div className="App">
       <Router>
-        <Main />
+        <Dashboard />
+        {/* <Main /> */}
         {/* <Sidebar /> */}
       </Router>
     </div>
--- front-end/src/components/dashboard/sidebar.js
+++ front-end/src/components/dashboard/sidebar.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "../../Style/Dashboard.css";
+import "../../Style/SideBar.css";
 
 const Sidebar = () => {
   return (
